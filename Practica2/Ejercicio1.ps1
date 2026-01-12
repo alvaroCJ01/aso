d42f92202cd7a8b187e0f09cec7945dd6032b4fc
alvaroCJ01 <alvcorjim@alu.edu.gva.es>
2026-01-12 08:33:54 +0100

    Add files via upload

---
+++ Practica2/Ejercicio1.ps1
@@ -0,0 +1,4 @@
+﻿Write-Output "Hola $env:USERNAME"
+Write-Output "Tu directorio de trabajo es $env:HOMEPATH"
+Write-Output "Perteneces al dominio $env:USERDOMAIN"
+Write-Output "Tu equipo se llama $env:COMPUTERNAME"
---
+++ Practica2/Ejercicio3.ps1
@@ -0,0 +1,4 @@
+﻿[int] $horas=Read-Host "Escribe el numero de horas trabajadas"
+[int] $coste=Read-Host "Escribe el precio de la hora"
+$sueldo= $horas * $coste
+Write-Output "Tu salario es de: $sueldo"

aee1ffd9519303c9cab5572fbd74d7a8006eba09
alvaroCJ01 <alvcorjim@alu.edu.gva.es>
2026-01-12 08:34:31 +0100

    Add files via upload

---
+++ Practica2_PowerShell/Ejercicio2.ps1
@@ -0,0 +1,4 @@
+﻿[int] $num1=Read-Host "Escribe el primer numero"
+[int] $num2=Read-Host "Escribe el segundo numero"
+$resultado = $num1 + $num2
+Write-Output "El resultado de la suma es: $resultado"

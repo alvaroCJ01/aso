3971f67d7a42b44a23ea0722d4e70d4b0e7432da
alvaroCJ01 <alvcorjim@alu.edu.gva.es>
2026-02-06 12:53:42 +0100

    Add files via upload

---
+++ ejercicio4.ps1
@@ -0,0 +1,6 @@
+﻿Import-Csv -Path "C:\Users\Administrador\archivos\empleados.csv" -Delimiter ";" | ForEach-Object {
+    $usuario = "$($_.nombre).$($_.apellido)"
+    if ($_.departamento -eq "PERSONAL") {
+           Set-ADUser -identity $usuario -ProfilePath \\WIN-7B6PU35SQJI\Empresa_users$\$usuario\
+    }
+}
